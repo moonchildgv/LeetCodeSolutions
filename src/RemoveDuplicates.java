fe84319975e8fa5b4e60a1e69b1db7ea4116cbad
moonchildgv <vono.giuseppe0@gmail.com>
2023-04-21 18:14:34 +0200

    add code

---
+++ src/RemoveDuplicates.java
@@ -0,0 +1,31 @@
+package Solution;
+
+public class RemoveDuplicates
+{
+    /*Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. 
+    The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+    Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+    -Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. 
+    The remaining elements of nums are not important as well as the size of nums.
+    -Return k.
+ */
+    public static int removeDuplicates(int[] nums)
+    {
+        int newIndex = 1;
+        for (int i = 0; i < nums.length - 1; i++)
+        {
+            if (nums[i] < nums[i+1]) 
+            { 
+                nums[newIndex] = nums[i+1];
+                newIndex++;
+            }
+        }
+        return newIndex;
+    }
+
+    public static void main(String args[])
+    {
+        int[] array = {1,1,2,2,3};
+        System.out.println(removeDuplicates(array));
+    }
+}
---
+++ src/SearchInsertPosition.java
@@ -0,0 +1,53 @@
+package Solution;
+
+public class SearchInsertPosition
+{
+    /* Given a sorted array of distinct integers and a target value, return the index if the target is found. 
+    If not, return the index where it would be if it were inserted in order. 
+    */
+    public static int searchInsert(int[] nums, int target) 
+    {
+        for(int i = 0; i < nums.length; i++)
+        {
+            if(nums[i] == target) return i;
+        }
+        int[] newArray = insertTarget(nums, target);
+        for(int i = 0; i < newArray.length; i++)
+        {
+            if(newArray[i] == target) return i;
+        }
+        return nums.length;
+    }
+
+    private static int[] insertTarget(int[] array, int target) {
+        int[] newArray = new int[array.length + 1];
+        int position = 0;
+        for(int i = 0; i < array.length; i++) 
+        {
+            if (target < array[i]) 
+            {
+                position = i;
+                break;
+            }
+            position = i + 1;
+        }
+        for(int i = 0; i < position; i++) 
+        {
+            newArray[i] = array[i];
+        }
+        newArray[position] = target; 
+        for(int i = position + 1; i < newArray.length; i++) 
+        {
+            newArray[i] = array[i - 1];
+        }
+        return newArray;
+    }
+
+    public static void main(String args[])
+    {
+        int[] array = {1,3,5,6};
+        System.out.print(searchInsert(array, 2));
+    }
+
+    
+}
