cfbf7cd21d9bc8a2e34aa7bdc835327ed6593cb1
moonchildgv <vono.giuseppe0@gmail.com>
2023-04-23 18:22:02 +0200

    fibo

--- src/ClimbingStairs.java
+++ src/ClimbingStairs.java
@@ -5,6 +5,7 @@ public class ClimbingStairs
     /* You are climbing a staircase. It takes n steps to reach the top.
     Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top? 
     */
+    /*FIBONACCI*/
     public static int climbStairs(int n)
     {
         if (n == 1) return n;
